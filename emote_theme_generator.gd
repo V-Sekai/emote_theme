3a6733e5a3ba23917ee90e890df604f1420522bb
Saracen <SaracenOne@gmail.com>
2018-07-29 03:32:55 +0100

    Initial commit.

---
+++ emote_theme_generator.gd
@@ -0,0 +1,9 @@
+extends EditorScript
+tool
+
+const emote_theme_const = preload("emote_theme.gd")
+#const extended_theme_const = preload("res://addons/extended_theme/extended_theme.gd")
+
+func _run():
+	var theme = emote_theme_const.generate_emote_theme(Theme, 1.0)
+	ResourceSaver.save("res://addons/emote_theme/emote_theme.tres", theme)
---
+++ emote_theme_plugin.gd
@@ -0,0 +1,5 @@
+extends EditorPlugin
+tool
+
+func get_name(): 
+	return "EmoteTheme"
