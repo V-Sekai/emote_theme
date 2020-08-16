d28a610cf908d1ccdf83e9fd8119be63d2c2aecf
Saracen <SaracenOne@gmail.com>
2020-08-16 22:13:35 +0100

    Add printed messages for initialisation and destruction.

--- emote_theme_plugin.gd
+++ emote_theme_plugin.gd
@@ -2,5 +2,15 @@ extends EditorPlugin
 tool
 
 
+func _init():
+	print("Initialising EmoteTheme plugin")
+
+
+func _notification(p_notification: int):
+	match p_notification:
+		NOTIFICATION_PREDELETE:
+			print("Destroying EmoteTheme plugin")
+
+
 func get_name():
 	return "EmoteTheme"
