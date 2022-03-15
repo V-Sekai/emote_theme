5b3e5be0607fa8d9f00e52977153905cc6628d75
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-03-15 07:41:12 -0700

    Code is not used anymore.

--- emote_theme_generator.gd
+++ emote_theme_generator.gd
@@ -4,8 +4,6 @@ extends EditorScript
 # Run this script in the editor.
 
 const emote_theme_const = preload("emote_theme.gd")
-#const extended_theme_const = preload("res://addons/extended_theme/extended_theme.gd")
-
 
 func _run():
 	var theme = emote_theme_const.generate_emote_theme(Theme, 1.0)
